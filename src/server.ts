498d985ccb42266b7c9ff64ff1a684e1cf815c1e
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-08 14:57:28 -0300

    feat: added flow to abort server if database is not connected

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
 import { app } from './app';
+import { prisma } from './database';
 import EnvProvider from './utils/EnvProvider';
 
-app.listen(EnvProvider.port, () => console.log(`Server started at port ${EnvProvider.port}`));
+async function main() {
+  try {
+    await prisma.$connect();
+    console.info('🔌 Database connected');
+
+    app.listen(EnvProvider.port, () => console.log(`✅ Server started at port ${EnvProvider.port}`));
+  } catch (err) {
+    console.error('⚠️ Failed to connect to the database', err);
+    console.warn('⚠️ Aborting the server');
+  }
+}
+
+main();
