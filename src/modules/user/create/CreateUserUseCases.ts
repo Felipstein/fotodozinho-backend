59b9c31b96a5ece74d3a5bb8d1693db13ed72293
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-08 16:40:28 -0300

    pause: pause of create user usecases creation

--- src/modules/user/create/CreateUserUseCases.ts
+++ src/modules/user/create/CreateUserUseCases.ts
@@ -1,5 +1,8 @@
+import { BadRequestError } from './../../../errors/BadRequestError';
 import { IUserView } from './../../../entities/IUserView';
 import { IUsersRepository } from './../../../repositories/IUsersRepository';
+import { someIsNullOrUndefined } from '../../../utils/Validate';
+
 import { CreateUserDTO } from './CreateUserDTO';
 
 export class CreateUserUseCases {
@@ -9,6 +12,15 @@ export class CreateUserUseCases {
   ) { }
 
   async execute({ name, email, phone, password }: CreateUserDTO): Promise<IUserView> {
+    if(someIsNullOrUndefined(name, email, password)) {
+      throw new BadRequestError('Preencha todos os campos obrigatórios');
+    }
+
+    const emailAlreadyExists = await this.usersRepository.listByEmail(email);
+    if(emailAlreadyExists) {
+      throw new BadRequestError('E-mail já em uso');
+    }
+
 
   }
 
