6daa1c43694cdc3453e3ecb98264eceffb401ea8
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-08 16:13:37 -0300

    feat: list user by id controller added

---
+++ src/modules/user/listById/ListUserByIdController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { ListUserByIdUseCases } from './ListUserByIdUseCases';
+
+export class ListUserByIdController {
+
+  constructor(
+    private listUserByIdUseCases: ListUserByIdUseCases,
+  ) { }
+
+  async handle(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+
+    const user = await this.listUserByIdUseCases.execute(id);
+
+    return res.json(user);
+  }
+
+}
