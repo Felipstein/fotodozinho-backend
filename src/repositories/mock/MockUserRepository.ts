9f5ff0b3bb6b0ab3defaf67afc9ad3a35e722655
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-08 18:11:16 -0300

    feat: removed password in return of user (fake database)

--- src/repositories/mock/MockUserRepository.ts
+++ src/repositories/mock/MockUserRepository.ts
@@ -1,4 +1,7 @@
+import { cloneDeep } from 'lodash';
+
 import { IUser } from '../../entities/IUser';
+import { uuidProvider } from '../../providers/UUID';
 import { IUsersRepository } from '../IUsersRepository';
 import { IUserView } from './../../entities/IUserView';
 
@@ -19,7 +22,15 @@ export class MockUserRepository implements IUsersRepository {
   }
 
   async create({ name, email, phone, password }: Omit<IUser, 'id'>): Promise<IUserView> {
+    const id = uuidProvider.generateCUID();
+    const user = { id, name, email, phone, password };
 
+    this.users.push(user);
+
+    const userWithoutPass = cloneDeep(user);
+    delete userWithoutPass.password;
+
+    return userWithoutPass;
   }
 
   async update(id: string, { name, email, password }: Omit<IUser, 'id'>): Promise<IUserView | null> {
