ab8a9f91d385dedc21a77c700ee0e8faeb5f45fc
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-08 16:15:05 -0300

    feat: added /users/id/:id route

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 
 import { listUsersFactory } from '../modules/user/listAll';
+import { listUserByIdFactory } from '../modules/user/listById';
 
 const route = Router();
 
 route.get('/', (req, res) => {
   return listUsersFactory().controller.handle(req, res);
+});
+
+route.get('/id/:id', (req, res) => {
+  return listUserByIdFactory().controller.handle(req, res);
 });
 
 export { route as userRoutes };
