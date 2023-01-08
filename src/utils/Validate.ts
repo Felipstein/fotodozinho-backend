14a00680530c8d60be41799d9976c62ff5c6114e
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-08 16:32:32 -0300

    feat: added function util to verify if some value is null or undefined

---
+++ src/utils/Validate.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function someIsNullOrUndefined(...values: any[]) {
+  const nullOrUndefined: any[] = [null, undefined];
+
+  if(nullOrUndefined.includes(values)) {
+    return true;
+  }
+
+  return values.some(nullOrUndefined.includes);
+}
