1f89687ebb4c8b0d3d7b7a7dc930f6b3816ce940
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-08 18:20:01 -0300

    feat: added fake databse

--- src/repositories/mock/MockUserRepository.ts
+++ src/repositories/mock/MockUserRepository.ts
@@ -1,5 +1,4 @@
 import { cloneDeep } from 'lodash';
-
 import { IUser } from '../../entities/IUser';
 import { uuidProvider } from '../../providers/UUID';
 import { IUsersRepository } from '../IUsersRepository';
@@ -7,18 +6,41 @@ import { IUserView } from './../../entities/IUserView';
 
 export class MockUserRepository implements IUsersRepository {
 
-  private readonly users: IUserView[];
+  private users: IUser[];
 
   async listAll(): Promise<IUserView[]> {
-    return this.users;
+    const users = this.users.map(user => {
+      const userWithoutPassword = cloneDeep(user);
+      delete userWithoutPassword.password;
+
+      return userWithoutPassword;
+    });
+
+    return users;
   }
 
   async listById(id: string): Promise<IUserView | null> {
-    return this.users.find(user => user.id === id) ?? null;
+    const user = this.users.find(user => user.id === id);
+    if(!user) {
+      return null;
+    }
+
+    const userWithoutPassword = cloneDeep(user);
+    delete userWithoutPassword.password;
+
+    return userWithoutPassword;
   }
 
   async listByEmail(email: string): Promise<IUserView | null> {
-    return this.users.find(user => user.email === email) ?? null;
+    const user = this.users.find(user => user.email === email);
+    if(!user) {
+      return null;
+    }
+
+    const userWithoutPassword = cloneDeep(user);
+    delete userWithoutPassword.password;
+
+    return userWithoutPassword;
   }
 
   async create({ name, email, phone, password }: Omit<IUser, 'id'>): Promise<IUserView> {
@@ -27,17 +49,30 @@ export class MockUserRepository implements IUsersRepository {
 
     this.users.push(user);
 
-    const userWithoutPass = cloneDeep(user);
-    delete userWithoutPass.password;
+    const userWithoutPassword = cloneDeep(user);
+    delete userWithoutPassword.password;
 
-    return userWithoutPass;
+    return userWithoutPassword;
   }
 
-  async update(id: string, { name, email, password }: Omit<IUser, 'id'>): Promise<IUserView | null> {
-    throw new Error('Method not implemented.');
+  async update(id: string, { name, email, phone, password }: Omit<IUser, 'id'>): Promise<IUserView | null> {
+    const newUser = { id, name, email, phone, password};
+
+    this.users = this.users.map(user => {
+      if(user.id === id) {
+        return newUser;
+      }
+
+      return user;
+    });
+
+    const userWithoutPassword = cloneDeep(newUser);
+    delete userWithoutPassword.password;
+
+    return userWithoutPassword;
   }
 
   delete(id: string): void {
-    throw new Error('Method not implemented.');
+    this.users = this.users.filter(user => user.id !== id);
   }
 }
