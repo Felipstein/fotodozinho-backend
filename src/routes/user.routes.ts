d6897a74e4d00fbd2d4f7e84b0d9016517b97a29
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-08 16:57:53 -0300

    feat: create user route added

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 
 import { listUsersFactory } from '../modules/user/listAll';
 import { listUserByIdFactory } from '../modules/user/listById';
+import { createUserFactory } from '../modules/user/create';
 
 const route = Router();
 
@@ -11,6 +12,10 @@ route.get('/', (req, res) => {
 
 route.get('/id/:id', (req, res) => {
   return listUserByIdFactory().controller.handle(req, res);
+});
+
+route.post('/', (req, res) => {
+  return createUserFactory().controller.handle(req, res);
 });
 
 export { route as userRoutes };
