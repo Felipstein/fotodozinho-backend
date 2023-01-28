fca4aa7fc64d9b06f51e393591b80ef0bd0e44a0
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-28 14:24:46 -0300

    test: added test to list all print prices

--- src/modules/print-price/listAll/ListPrintPrices.test.ts
+++ src/modules/print-price/listAll/ListPrintPrices.test.ts
@@ -1,3 +1,4 @@
+import { IPrintPrice } from './../../../entities/print-price/IPrintPrice';
 import { MockPrintPricesRepository } from '../../../repositories/print-prices/MockPrintPricesRepository';
 import { CreatePrintPriceUseCases } from '../create/CreatePrintPriceUseCases';
 import { ListPrintPricesUseCases } from './ListPrintPricesUseCases';
@@ -26,6 +27,22 @@ describe('List all Print Prices', () => {
     const printPrices = await listPrintPricesUseCases.execute();
 
     expect(printPrices).toHaveLength(2);
+  });
+
+  it('should exactly two print prices', async () => {
+    const printPrice1 = await createPrintPriceUseCases.execute({
+      length: '10x15',
+      price: 5,
+    });
+    const printPrice2 = await createPrintPriceUseCases.execute({
+      length: '15x21',
+      price: 10,
+    });
+
+    const printPrices = await listPrintPricesUseCases.execute();
+
+    expect(printPrices).toContainEqual({ id: printPrice1.id, length: printPrice1.length, price: printPrice1.price });
+    expect(printPrices).toContainEqual({ id: printPrice2.id, length: printPrice2.length, price: printPrice2.price });
   });
 
 });
--- src/routes/print-price.routes.ts
+++ src/routes/print-price.routes.ts
@@ -4,7 +4,7 @@ import { createPrintPriceFactory } from '../modules/print-price/create';
 const route = Router();
 
 route.post('/', (req, res) => {
-  createPrintPriceFactory().controller.handle(req, res);
+  return createPrintPriceFactory().controller.handle(req, res);
 });
 
 export { route as printPriceRoutes };
