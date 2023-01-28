eaa28983294b88eb670bd19b31d5634812c2dde4
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-28 15:34:34 -0300

    test: added tests to update price of print price

---
+++ src/modules/print-price/updatePrice/UpdatePricePrintPrice.test.ts
@@ -0,0 +1,43 @@
+import { PrintPriceNotFound } from '../../../errors/PrintPriceNotFoundError';
+import { RequiredFieldsError } from '../../../errors/RequiredFieldsError';
+import { MockPrintPricesRepository } from '../../../repositories/print-prices/MockPrintPricesRepository';
+import { CreatePrintPriceUseCases } from '../create/CreatePrintPriceUseCases';
+import { UpdatePricePrintPriceUseCases } from './UpdatePricePrintPriceUseCases';
+
+describe('Update Price of Print Price', () => {
+
+  const printPricesRepository = new MockPrintPricesRepository();
+  const createPrintPriceUseCases = new CreatePrintPriceUseCases(printPricesRepository);
+  const updatePricePrintPriceUseCases = new UpdatePricePrintPriceUseCases(printPricesRepository);
+
+  afterEach(() => {
+    printPricesRepository.cleanRepository();
+  });
+
+  it('should update the price of print price', async () => {
+    const { id } = await createPrintPriceUseCases.execute({
+      length: '15x21',
+      price: 5,
+    });
+
+    const printPriceUpdated = await updatePricePrintPriceUseCases.execute(id, { price: 10 });
+
+    expect(printPriceUpdated.price).toBe(10);
+  });
+
+  it('should throw an error when not passing the price which should be updated', async () => {
+    const { id } = await createPrintPriceUseCases.execute({
+      length: '10x15',
+      price: 5,
+    });
+
+    // @ts-ignore
+    expect(updatePricePrintPriceUseCases.execute(id, {})).rejects.toThrow(RequiredFieldsError);
+  });
+
+  it('should throw an error when update the price of print price that does not exists', async () => {
+
+    expect(updatePricePrintPriceUseCases.execute('unknowid', { price: 5 })).rejects.toThrow(PrintPriceNotFound);
+  });
+
+});
