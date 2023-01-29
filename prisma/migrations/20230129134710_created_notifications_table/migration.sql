93d9946fa1ff5626abcc19d4f2cbfb08dc294901
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-29 10:47:17 -0300

    feat: added notification table

---
+++ prisma/migrations/20230129134710_created_notifications_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "read" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
