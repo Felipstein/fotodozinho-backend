7e4ab8ea38630022ed52735c330a184e5c96d531
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-29 18:21:43 -0300

    feat: added update read notification use case

---
+++ src/modules/notification/updateRead/UpdateReadNotificationUseCases.ts
@@ -0,0 +1,32 @@
+import { INotification } from '../../../entities/notification/INotification';
+import { IDNotGivenError } from '../../../errors/IDNotGivenError';
+import { NotificationNotFoundError } from '../../../errors/NotificationNotFoundError';
+import { RequiredFieldsError } from '../../../errors/RequiredFieldsError';
+import { INotificationsRepository } from './../../../repositories/notifications/INotificationsRepository';
+import { UpdateReadNotificationDTO } from './UpdateReadNotificationDTO';
+export class UpdateReadNotificationUseCases {
+
+  constructor(
+    private notificationsRepository: INotificationsRepository,
+  ) { }
+
+  async execute(id: string, { read }: UpdateReadNotificationDTO): Promise<INotification> {
+    if(!id) {
+      throw new IDNotGivenError();
+    }
+
+    if(!read) {
+      throw new RequiredFieldsError('Lida');
+    }
+
+    const notificationExists = await this.notificationsRepository.listById(id);
+    if(!notificationExists) {
+      throw new NotificationNotFoundError();
+    }
+
+    const notification = await this.notificationsRepository.updateRead(id, { read });
+
+    return notification;
+  }
+
+}
