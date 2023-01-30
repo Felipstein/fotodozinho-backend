eba601437e9de8bb7032fb8b4892e933968c79f3
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-01-29 23:16:18 -0300

    feat and test: finished use cases of create print order and tests

--- src/modules/print-order/create/CreatePrintOrder.test.ts
+++ src/modules/print-order/create/CreatePrintOrder.test.ts
@@ -1,5 +1,7 @@
 import { IPrintOrder } from '../../../entities/print-order/IPrintOrder';
 import { IPrintCreation } from '../../../entities/print-order/print/IPrintCreation';
+import { BadRequestError } from '../../../errors/BadRequestError';
+import { UserNotFoundError } from '../../../errors/UserNotFoundError';
 import { MockColorsRepository } from '../../../repositories/colors/MockColorsRepository';
 import { MockPrintOrdersRepository } from '../../../repositories/print-orders/MockPrintOrdersRepository';
 import { MockPrintPricesRepository } from '../../../repositories/print-prices/MockPrintPricesRepository';
@@ -12,11 +14,13 @@ describe('Create Print Order', () => {
   const usersRepository = new MockUserRepository();
   const printPricesRepository = new MockPrintPricesRepository();
   const colorsRepository = new MockColorsRepository();
-  const createPrintOrderUseCases = new CreatePrintOrderUseCases(printOrdersRepository, usersRepository);
+  const createPrintOrderUseCases = new CreatePrintOrderUseCases(printOrdersRepository, usersRepository, printPricesRepository, colorsRepository);
 
   afterEach(() => {
     printOrdersRepository.cleanRepository();
     usersRepository.cleanRepository();
+    printPricesRepository.cleanRepository();
+    colorsRepository.cleanRepository();
   });
 
   it('should create new print order', async () => {
@@ -45,7 +49,7 @@ describe('Create Print Order', () => {
       },
     ];
 
-    const printOrder = await createPrintOrderUseCases.execute({
+    const { printOrder } = await createPrintOrderUseCases.execute({
       prints, userId,
     });
 
@@ -69,6 +73,132 @@ describe('Create Print Order', () => {
       userId,
       createdAt: printOrder.createdAt,
     } as IPrintOrder);
+  });
+
+  it('should throw an error when create print order with user that does not exists', async () => {
+    const printPrice = await printPricesRepository.create({ length: '10x15', price: 5 });
+    const color = await colorsRepository.create({ color: 'red' });
+
+    const prints: IPrintCreation[] = [
+      {
+        imageName: 'Image Name.jpeg',
+        imageUrl: 'http://example.com/key-image-name.jpeg',
+        key: 'key-image-name.jpeg',
+        printPrice,
+        printPriceId: printPrice.id,
+        border: false,
+        color,
+        colorId: color.id,
+        quantity: 1,
+      },
+    ];
+
+    expect(() => createPrintOrderUseCases.execute({ prints, userId: 'fake-user-id' })).rejects.toThrow(UserNotFoundError);
+  });
+
+  it('should throw an error when create print order without prints', async () => {
+    const { id: userId } = await usersRepository.create({
+      name: 'User Test',
+      email: 'test@test.com',
+      password: '123456',
+      phone: '99999999999',
+      admin: false,
+    });
+
+    const createPrintOrder = () => createPrintOrderUseCases.execute({ prints: [], userId });
+
+    expect(createPrintOrder).rejects.toThrow(BadRequestError);
+    expect(createPrintOrder).rejects.toThrow('Nenhuma foto para revelação foi enviada');
+  });
+
+  it('should throw an error when create print order with all wrong prints', async () => {
+    const { id: userId } = await usersRepository.create({
+      name: 'User Test',
+      email: 'test@test.com',
+      password: '123456',
+      phone: '99999999999',
+      admin: false,
+    });
+
+    const printPrice = await printPricesRepository.create({ length: '10x15', price: 5 });
+    const color = await colorsRepository.create({ color: 'red' });
+
+    const prints = [
+      {
+        // imageName: 'Image Name.jpeg',
+        imageUrl: 'http://example.com/key-image-name.jpeg',
+        // key: 'key-image-name.jpeg',
+        printPrice,
+        printPriceId: printPrice.id,
+        border: false,
+        color,
+        colorId: color.id,
+        quantity: 1,
+      },
+      {
+        imageName: 'Image Name.jpeg',
+        // imageUrl: 'http://example.com/key-image-name.jpeg',
+        key: 'key-image-name.jpeg',
+        printPrice,
+        printPriceId: 'fake-print-price-id',
+        border: false,
+        color,
+        colorId: color.id,
+        quantity: 'notnumber',
+      },
+    ];
+
+    // @ts-ignore
+    const createPrintOrder = () => createPrintOrderUseCases.execute({ prints, userId });
+
+    expect(createPrintOrder).rejects.toThrow(BadRequestError);
+    expect(createPrintOrder).rejects.toThrow('Nenhuma foto para revelação possui seus campos corretos ou preenchidos');
+  });
+
+  it('should return one rejected print because wrong attributes and one accepted print', async () => {
+    const { id: userId } = await usersRepository.create({
+      name: 'User Test',
+      email: 'test@test.com',
+      password: '123456',
+      phone: '99999999999',
+      admin: false,
+    });
+
+    const printPrice = await printPricesRepository.create({ length: '10x15', price: 5 });
+    const color = await colorsRepository.create({ color: 'red' });
+
+    const prints = [
+      {
+        imageName: 'Image Name.jpeg',
+        imageUrl: 'http://example.com/key-image-name.jpeg',
+        key: 'key-image-name.jpeg',
+        printPrice,
+        printPriceId: printPrice.id,
+        border: false,
+        color,
+        colorId: color.id,
+        quantity: 1,
+      },
+      {
+        imageName: 'Image Name.jpeg',
+        imageUrl: 'http://example.com/key-image-name.jpeg',
+        // key: 'key-image-name.jpeg',
+        printPrice,
+        printPriceId: 'fake-print-price-id',
+        border: false,
+        color,
+        colorId: color.id,
+        quantity: 'notnumber',
+      },
+    ];
+
+    const { printOrder, rejectedPrints } = await createPrintOrderUseCases.execute({
+      // @ts-ignore
+      prints, userId
+    });
+
+    expect(printOrder.prints).toHaveLength(1);
+    expect(rejectedPrints).toHaveLength(1);
   });
 
 });
--- src/modules/print-order/create/CreatePrintOrderUseCases.ts
+++ src/modules/print-order/create/CreatePrintOrderUseCases.ts
@@ -1,8 +1,11 @@
 import { IPrintOrder } from '../../../entities/print-order/IPrintOrder';
+import { IPrintCreation } from '../../../entities/print-order/print/IPrintCreation';
 import { BadRequestError } from '../../../errors/BadRequestError';
 import { RequiredFieldsError } from '../../../errors/RequiredFieldsError';
 import { UserNotFoundError } from '../../../errors/UserNotFoundError';
+import { IColorsRepository } from '../../../repositories/colors/IColorsRepository';
 import { IPrintOrdersRepository } from '../../../repositories/print-orders/IPrintOrdersRepository';
+import { IPrintPricesRepository } from '../../../repositories/print-prices/IPrintPricesRepository';
 import { IUsersRepository } from '../../../repositories/users/IUsersRepository';
 import { someIsNullOrUndefined } from '../../../utils/Validate';
 import { CreatePrintOrderDTO } from './CreatePrintOrderDTO';
@@ -12,9 +15,11 @@ export class CreatePrintOrderUseCases {
   constructor(
     private printOrdersRepository: IPrintOrdersRepository,
     private usersRepository: IUsersRepository,
+    private printPricesRepository: IPrintPricesRepository,
+    private colorsRepository: IColorsRepository,
   ) { }
 
-  async execute({ userId, prints }: CreatePrintOrderDTO): Promise<IPrintOrder> {
+  async execute({ userId, prints }: CreatePrintOrderDTO): Promise<{ printOrder: IPrintOrder, rejectedPrints: IPrintCreation[] }> {
     if(someIsNullOrUndefined(userId, prints)) {
       throw new RequiredFieldsError('Usuário', 'Fotos para revelação');
     }
@@ -28,11 +33,60 @@ export class CreatePrintOrderUseCases {
       throw new UserNotFoundError();
     }
 
+    const { acceptedPrints, rejectedPrints } = await this.validatePrints(prints);
+
+    if(acceptedPrints.length === 0) {
+      throw new BadRequestError('Nenhuma foto para revelação possui seus campos corretos ou preenchidos');
+    }
+
     const printOrder = await this.printOrdersRepository.create({
-      userId, prints,
+      userId, prints: acceptedPrints,
     });
 
-    return printOrder;
+    return { printOrder, rejectedPrints };
+  }
+
+  private async validatePrints(prints: IPrintCreation[]): Promise<{ acceptedPrints: IPrintCreation[], rejectedPrints: IPrintCreation[] }> {
+    const acceptedPrints: IPrintCreation[] = [];
+    const rejectedPrints: IPrintCreation[] = [];
+
+    const promises = prints.map(async print => {
+      if(someIsNullOrUndefined(
+        print.imageName,
+        print.imageUrl,
+        print.key,
+        print.printPriceId,
+        print.border,
+        print.colorId,
+        print.quantity,
+      )) {
+        rejectedPrints.push(print);
+        return;
+      }
+
+      if(isNaN(print.quantity)) {
+        rejectedPrints.push(print);
+        return;
+      }
+
+      const printPricesExists = await this.printPricesRepository.listById(print.printPriceId);
+      if(!printPricesExists) {
+        rejectedPrints.push(print);
+        return;
+      }
+
+      const colorExists = await this.colorsRepository.listById(print.colorId);
+      if(!colorExists) {
+        rejectedPrints.push(print);
+        return;
+      }
+
+      acceptedPrints.push(print);
+    });
+
+    await Promise.all(promises);
+
+    return { acceptedPrints, rejectedPrints };
   }
 
 }
