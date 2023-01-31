c846e83a93278e17dcfe79dd9c8007dba84abb0a
Felipstein <felipstein.oliveira@gmail.com>
2023-01-31 16:58:51 -0300

    feat: added GET /print-orders route

--- src/routes/print-order.routes.ts
+++ src/routes/print-order.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import { createPrintOrderFactory } from '../modules/print-order/create';
+import { listPrintOrdersFactory } from '../modules/print-order/listAll';
 
 const route = Router();
+
+route.get('/', (req, res) => {
+  return listPrintOrdersFactory().controller.handle(req, res);
+});
 
 route.post('/', (req, res) => {
   return createPrintOrderFactory().controller.handle(req, res);
