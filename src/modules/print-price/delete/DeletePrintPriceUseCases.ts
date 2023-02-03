fb2bb3bde8c958f4fe6621aecf4f9ec6766bb199
Felipstein <felipstein.oliveira@gmail.com>
2023-02-03 14:54:22 -0300

    fix: can't delete print price if is used in print

--- src/modules/print-price/delete/DeletePrintPriceUseCases.ts
+++ src/modules/print-price/delete/DeletePrintPriceUseCases.ts
@@ -1,11 +1,14 @@
+import { ConflictRequestError } from '../../../errors/ConflictRequestError';
 import { IDNotGivenError } from '../../../errors/IDNotGivenError';
 import { PrintPriceNotFound } from '../../../errors/PrintPriceNotFoundError';
 import { IPrintPricesRepository } from '../../../repositories/print-prices/IPrintPricesRepository';
+import { IPrintsRepository } from '../../../repositories/prints/IPrintsRepository';
 
 export class DeletePrintPriceUseCases {
 
   constructor(
     private printPricesRepository: IPrintPricesRepository,
+    private printsRepository: IPrintsRepository,
   ) { }
 
   async execute(id: string): Promise<void> {
@@ -13,9 +16,14 @@ export class DeletePrintPriceUseCases {
       throw new IDNotGivenError();
     }
 
-    const printPriceExists = await this.printPricesRepository.listById(id);
-    if(!printPriceExists) {
+    const printPrice = await this.printPricesRepository.listById(id);
+    if(!printPrice) {
       throw new PrintPriceNotFound();
+    }
+
+    const prints = await this.printsRepository.listByPrintPriceId(id);
+    if(prints.length > 0) {
+      throw new ConflictRequestError('Não é possível deletar essa categoria de tamanho/tipo. Existe uma ou mais fotos para serem impressas que a utilizam.');
     }
 
     await this.printPricesRepository.delete(id);
