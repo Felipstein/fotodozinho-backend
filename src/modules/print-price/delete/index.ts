2b7387446b0c2958319ace147d75ddfc33159cd4
Felipstein <felipstein.oliveira@gmail.com>
2023-02-03 15:28:32 -0300

    fix: providing prints repository to delete print price use case

--- src/modules/print-price/delete/index.ts
+++ src/modules/print-price/delete/index.ts
@@ -1,9 +1,9 @@
-import { currentPrintPricesRepository } from '../../../repositories';
+import { currentPrintPricesRepository, currentPrintsRepository } from '../../../repositories';
 import { DeletePrintPriceController } from './DeletePrintPriceController';
 import { DeletePrintPriceUseCases } from './DeletePrintPriceUseCases';
 
 export function deletePrintPriceFactory() {
-  const deletePrintPriceUseCases = new DeletePrintPriceUseCases(currentPrintPricesRepository);
+  const deletePrintPriceUseCases = new DeletePrintPriceUseCases(currentPrintPricesRepository, currentPrintsRepository);
   const deletePrintPriceController = new DeletePrintPriceController(deletePrintPriceUseCases);
 
   return { useCases: deletePrintPriceUseCases, controller: deletePrintPriceController };
