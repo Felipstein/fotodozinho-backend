c54396966e98668c8197b876813d5b4c18f44387
Felipstein <felipstein.oliveira@gmail.com>
2023-02-08 13:54:26 -0300

    feat: added list all products use case

---
+++ src/modules/product/ListProductsUseCases.ts
@@ -0,0 +1,16 @@
+import { IProduct } from '../../entities/product/IProduct';
+import { IProductsRepository } from '../../repositories/product/IProductsRepository';
+
+export class ListProductsUseCases {
+
+  constructor(
+    private productsRepository: IProductsRepository,
+  ) { }
+
+  async execute(): Promise<IProduct[]> {
+    const products = await this.productsRepository.listAll();
+
+    return products;
+  }
+
+}
