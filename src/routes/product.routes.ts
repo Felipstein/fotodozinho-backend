f008715c281d9967762d3ed42c7ee3a7d1ea74df
Felipstein <felipstein.oliveira@gmail.com>
2023-02-08 17:54:26 -0300

    feat: added DELETE /products/:id route

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { deleteProductFactory } from '../modules/product/delete';
 import { listProductsFactory } from '../modules/product/listAll';
 import { listProductByIdFactory } from '../modules/product/listById';
 
@@ -10,6 +11,10 @@ route.get('/', (req, res) => {
 
 route.get('/:id', (req, res) => {
   return listProductByIdFactory().controller.handle(req, res);
+});
+
+route.delete('/:id', (req, res) => {
+  return deleteProductFactory().controller.handle(req, res);
 });
 
 export { route as productRoutes };
