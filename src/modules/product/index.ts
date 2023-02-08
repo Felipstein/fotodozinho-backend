9b4fce058821630ba2e55e73469dc90d0a12bcb4
Felipstein <felipstein.oliveira@gmail.com>
2023-02-08 14:19:49 -0300

    feat: added list all products controller

---
+++ src/modules/product/index.ts
@@ -0,0 +1,10 @@
+import { currentProductsRepository } from '../../repositories';
+import { ListProductsController } from './ListProductsController';
+import { ListProductsUseCases } from './ListProductsUseCases';
+
+export function listProductsFactory() {
+  const useCases = new ListProductsUseCases(currentProductsRepository);
+  const controller = new ListProductsController(useCases);
+
+  return { useCases, controller };
+}
