54c67b7b3a04a97297ca47fd4b29da61e0f19883
Felipstein <felipstein.oliveira@gmail.com>
2023-02-13 18:00:38 -0300

    pauuses to continue in other desktop

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
+import { NextFunction, Request, Response, Router } from 'express';
 import { createProductFactory } from '../modules/product/create';
 import { deleteProductFactory } from '../modules/product/delete';
 import { listProductsFactory } from '../modules/product/listAll';
 import { listProductByIdFactory } from '../modules/product/listById';
 import { updateProductFactory } from '../modules/product/update';
 import { uploadProvider } from '../providers/Upload';
+import { ImageDeleteService } from '../services/image-delete';
+import { StorageType } from '../services/image-storaged-type';
 
 const route = Router();
 
@@ -18,6 +20,20 @@ route.get('/:id', (req, res) => {
 
 route.post('/', uploadProvider.uploadSingleFile('fileImage'), (req, res) => {
   return createProductFactory().controller.handle(req, res);
+}, (error: any, req: Request, res: Response, next: NextFunction) => {
+  if(req.file) {
+    const { filename: keyLocal } = req.file;
+    const { key: keyS3 } = req.file as unknown as { key: string };
+
+    const key = keyS3 || keyLocal;
+
+    if(key) {
+      ImageDeleteService.deleteImage(key, StorageType.S3);
+      ImageDeleteService.deleteImage(key, StorageType.LOCAL);
+    }
+  }
+
+  throw error;
 });
 
 route.put('/:id', uploadProvider.uploadSingleFile('fileImage'), (req, res) => {
