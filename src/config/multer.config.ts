3d6f3b122998f7f20a7eabd2e35df22a1ab49139
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-13 00:37:56 -0300

    feat: added aws credentials (to .env and multer config)

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -1,9 +1,12 @@
 import path from 'path';
 import multer from 'multer';
 import crypto from 'crypto';
+import multerS3 from 'multer-s3';
+
 import { BadRequestError } from '../errors/BadRequestError';
 import { InternalServerError } from '../errors/InternalServerError';
 import EnvProvider from '../utils/EnvProvider';
+import { S3Client } from '@aws-sdk/client-s3';
 
 const localPath = path.resolve(__dirname, '..', '..', 'tmp', 'uploads');
 
@@ -39,7 +42,34 @@ const storageType = {
     }
   }),
 
-  s3: {},
+  s3: multerS3({
+    s3: new S3Client({
+      region: EnvProvider.aws.region,
+      credentials: {
+        accessKeyId: EnvProvider.aws.accessKeyId,
+        secretAccessKey: EnvProvider.aws.secretAccessKey,
+      },
+    }),
+
+    bucket: EnvProvider.aws.bucketName,
+
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+
+    key(req, file, callback) {
+      crypto.randomBytes(6, (err, hash) => {
+        const time = Date.now();
+
+        const fileName = `${time}-${hash.toString('hex')}_${file.originalname}`;
+
+        if(err) {
+          callback(new InternalServerError(), fileName);
+        }
+
+        callback(null, fileName);
+      });
+    },
+  }),
 };
 
 export default {
--- src/utils/EnvProvider.ts
+++ src/utils/EnvProvider.ts
@@ -2,4 +2,10 @@ export default {
   host: process.env.HOST,
   port: process.env.PORT,
   storageType: process.env.STORAGE_TYPE,
+  aws: {
+    bucketName: process.env.AWS_BUCKET_NAME,
+    region: process.env.AWS_DEFAULT_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
 };
