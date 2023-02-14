091b5e5b7f162b09f6387d17a5d2c8938a24dc9b
Felipstein <felipstein.oliveira@gmail.com>
2023-02-14 17:46:24 -0300

    feat: added image storaged type to creation of print

--- src/modules/print/create/CreatePrintUseCases.ts
+++ src/modules/print/create/CreatePrintUseCases.ts
@@ -10,6 +10,8 @@ import { IColorsRepository } from '../../../repositories/colors/IColorsRepositor
 import { IPrintOrdersRepository } from '../../../repositories/print-orders/IPrintOrdersRepository';
 import { IPrintPricesRepository } from '../../../repositories/print-prices/IPrintPricesRepository';
 import { IPrintsRepository } from '../../../repositories/prints/IPrintsRepository';
+import { EnvProvider } from '../../../services/env-provider';
+import { ImageStoragedService } from '../../../services/image-storaged-type';
 import { ParseBoolean } from '../../../services/parse-boolean';
 import { ValidateService } from '../../../services/validate';
 
@@ -22,7 +24,7 @@ export class CreatePrintUseCases {
     private printPricesRepository: IPrintPricesRepository,
   ) { }
 
-  async execute({ imageName, imageUrl, key, border, colorId, printPriceId, quantity, printOrderId }: PrintCreateRequest): Promise<IPrint> {
+  async execute({ imageName, imageUrl, key, border, colorId, printPriceId, quantity, printOrderId }: Omit<PrintCreateRequest, 'imageStoragedType'>): Promise<IPrint> {
     if(ValidateService.someIsNullOrUndefined(imageName, imageUrl, key, border, colorId, printPriceId, quantity, printOrderId)) {
       throw new RequiredFieldsError('Nome da imagem', 'URL da imagem', 'Imagem', 'Borda', 'Cor/tipo', 'Preço/tamanho', 'Quantidade', 'Identificador do pedido');
     }
@@ -51,8 +53,10 @@ export class CreatePrintUseCases {
       throw new BadRequestError('Esse pedido foto já está registrado.');
     }
 
+    const imageStoragedType = ImageStoragedService.convertStorageTypeFormat(EnvProvider.storageType);
+
     const print = await this.printsRepository.create({
-      imageName, imageUrl, key, border: ParseBoolean.parse(border), colorId, printPriceId, quantity, printOrderId,
+      imageName, imageUrl, key, imageStoragedType, border: ParseBoolean.parse(border), colorId, printPriceId, quantity, printOrderId,
     });
 
     return print;
