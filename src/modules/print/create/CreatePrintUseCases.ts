9f73c80ca666d8e594dcfb561386257970ac6d8d
Felipstein <felipstein.oliveira@gmail.com>
2023-02-14 17:27:03 -0300

    refactor: removed unused import

--- src/modules/print/create/CreatePrintUseCases.ts
+++ src/modules/print/create/CreatePrintUseCases.ts
@@ -1,4 +1,3 @@
-import { prisma } from '../../../database';
 import { PrintCreateRequest } from '../../../entities/print-order/print/dtos/PrintCreateRequest';
 import { IPrint } from '../../../entities/print-order/print/IPrint';
 import { BadRequestError } from '../../../errors/BadRequestError';
