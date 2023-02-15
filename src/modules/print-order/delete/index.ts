c80a563b1cbe82e13f490f1c87bea98b11dc9c08
Felipstein <felipstein.oliveira@gmail.com>
2023-02-15 10:55:47 -0300

    fix: providing prints repository to delete print use cases

--- src/modules/print-order/delete/index.ts
+++ src/modules/print-order/delete/index.ts
@@ -1,9 +1,9 @@
-import { currentPrintOrdersRepository } from '../../../repositories';
+import { currentPrintOrdersRepository, currentPrintsRepository } from '../../../repositories';
 import { DeletePrintOrderController } from './DeletePrintOrderController';
 import { DeletePrintOrderUseCases } from './DeletePrintOrderUseCases';
 
 export function deletePrintOrderFactory() {
-  const deletePrintOrderUseCases = new DeletePrintOrderUseCases(currentPrintOrdersRepository);
+  const deletePrintOrderUseCases = new DeletePrintOrderUseCases(currentPrintOrdersRepository, currentPrintsRepository);
   const deletePrintOrderController = new DeletePrintOrderController(deletePrintOrderUseCases);
 
   return { useCases: deletePrintOrderUseCases, controller: deletePrintOrderController };
