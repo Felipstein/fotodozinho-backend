faaf124902f21854e2fc2a79b5cc595e403dcbe1
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-16 00:55:25 -0300

    feat(sign in): added routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import { signUpFactory } from '../modules/auth/signup';
+import { signInFactory } from '../modules/auth/signin';
 
 const route = Router();
+
+route.post('/signin', (req, res) => {
+  return signInFactory().controller.handle(req, res);
+});
 
 route.post('/signup', (req, res) => {
   return signUpFactory().controller.handle(req, res);
