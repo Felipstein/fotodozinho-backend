95f1797a14e93b834e9b4f33d91fce0e6eaafe09
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-16 01:51:59 -0300

    refactor: added unused imports and test route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -11,16 +11,11 @@ import { productRoutes } from './product.routes';
 import { shoppingCartRoutes } from './shopping-cart.routes';
 import { listShoppingCartsFactory } from '../modules/shopping-cart/listAll';
 import { ensureShoppingCartUser } from '../middlewares/ensureShoppingCartUser';
-import { currentRevokedTokensRepository, currentShoppingCartsRepository, currentUsersRepository } from '../repositories';
+import { currentShoppingCartsRepository, currentUsersRepository } from '../repositories';
 import { purchaseOrderRoutes } from './purchase-order.routes';
 import { failedImageUploadedRoutes } from './failed-image-uploaded.routes';
 import { printRoutes } from './print.routes';
 import { authRoutes } from './auth.routes';
-import { UnauthorizedError } from '../errors/UnauthorizedError';
-import { BadRequestError } from '../errors/BadRequestError';
-import { InvalidTokenError } from '../errors/InvalidTokenError';
-import { tokenProvider } from '../providers/Token';
-import { RevokedTokenError } from '../errors/RevokedTokenError';
 
 const injectUserId = ensureShoppingCartUser(currentShoppingCartsRepository, currentUsersRepository);
 
@@ -49,27 +44,5 @@ routes.get('/shopping-carts', (req, res) => {
 routes.use('/purchase-orders', purchaseOrderRoutes);
 
 routes.use('/failed-images-uploaded', failedImageUploadedRoutes);
-
-routes.get('/test', async (req, res) => {
-
-  const authorization = req.headers.authorization;
-  if(!authorization) {
-    throw new UnauthorizedError();
-  }
-
-  if(typeof authorization !== 'string') {
-    throw new UnauthorizedError();
-  }
-
-  const [bearer, token] = authorization.split(' ');
-
-  if(bearer !== 'Bearer') {
-    throw new InvalidTokenError();
-  }
-
-  await tokenProvider.verify(token);
-
-  return res.sendStatus(200);
-});
 
 export { routes };
