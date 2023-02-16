ef328be6e0704af5b9ae26aa179ae372d6d99feb
Felipstein <felipstein.oliveira@gmail.com>
2023-02-16 16:21:05 -0300

    feat: added all permissions to admin user

--- src/middlewares/ensureSelfAction.ts
+++ src/middlewares/ensureSelfAction.ts
@@ -1,16 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
 import { ForbiddenError } from '../errors/ForbiddenError';
 import { UnauthorizedError } from '../errors/UnauthorizedError';
+import { currentUsersRepository } from '../repositories';
 
 export async function ensureSelfAction(req: Request, res: Response, next: NextFunction) {
-  const { userId: userIdOfAction } = req.params;
+  const { userId: userIdRequesting } = req.params;
   const { userId: userIdAuthenticated } = req;
 
   if(!userIdAuthenticated) {
     throw new UnauthorizedError();
   }
 
-  if(userIdOfAction !== userIdAuthenticated) {
+  const userRequesting = await currentUsersRepository.listById(userIdRequesting);
+  if(!userIdRequesting) {
+    throw new UnauthorizedError();
+  }
+
+  if(userRequesting.admin) {
+    return next();
+  }
+
+  if(userIdRequesting !== userIdAuthenticated) {
     throw new ForbiddenError();
   }
 
