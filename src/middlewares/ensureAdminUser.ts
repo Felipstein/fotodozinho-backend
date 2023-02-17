0b545c9c352e1f17b313b42e6e33952507b58b85
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-17 09:44:28 -0300

    fix: returning 404 not found to admin routes

--- src/middlewares/ensureAdminUser.ts
+++ src/middlewares/ensureAdminUser.ts
@@ -5,19 +5,19 @@ import { NotFoundHTMLTemplate } from '../shared/NotFoundHTMLTemplate';
 export async function ensureAdminUser(req: Request, res: Response, next: NextFunction) {
   const { userId } = req;
 
-  const html = NotFoundHTMLTemplate.getHTML(req.url, req.method);
+  const html = NotFoundHTMLTemplate.getHTML(req.originalUrl, req.method);
 
   if(!userId) {
-    return res.status(204).send(html);
+    return res.status(404).send(html);
   }
 
   const user = await currentUsersRepository.listById(userId);
   if(!user) {
-    return res.status(204).send(html);
+    return res.status(404).send(html);
   }
 
   if(!user.admin) {
-    return res.status(204).send(html);
+    return res.status(404).send(html);
   }
 
   next();
