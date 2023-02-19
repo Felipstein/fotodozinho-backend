ca8ac9cc47a575d6c077b62c21072ec4a79cc2c5
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-19 16:12:28 -0300

    refactor: added repeatly code between IUserView and IUser entities

--- src/entities/user/IUserView.ts
+++ src/entities/user/IUserView.ts
@@ -1,12 +1,3 @@
-export interface IUserView {
-  id: string;
-  name: string;
-  email: string;
-  phone?: string | null;
-  createdAt: Date;
-  admin: boolean;
-  totalPrints: number;
-  totalPrintOrders: number;
-  totalPurchases: number;
-  totalPurchaseOrders: number;
-}
+import { IUser } from './IUser';
+
+export type IUserView = Omit<IUser, 'password'>;
