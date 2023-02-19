c8421c85684fdc26aa2feefc9b853ffdab75c3cf
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-19 19:04:55 -0300

    feat: added filter when to print order use case

--- src/modules/print-order/listAll/ListPrintOrdersController.ts
+++ src/modules/print-order/listAll/ListPrintOrdersController.ts
@@ -10,8 +10,9 @@ export class ListPrintOrdersController {
 
   async handle(req: Request, res: Response): Promise<Response> {
     const status = req.query.status as PrintOrderStatus | undefined;
+    const when = req.query.when as 'today' | 'lastweek' | 'lastmonth' | undefined;
 
-    const printOrders = await this.listPrintOrdersUseCases.execute(status);
+    const printOrders = await this.listPrintOrdersUseCases.execute(status, when);
 
     return res.json(printOrders);
   }
--- src/modules/print-order/listAll/ListPrintOrdersUseCases.ts
+++ src/modules/print-order/listAll/ListPrintOrdersUseCases.ts
@@ -2,6 +2,7 @@ import { convertPrintOrderStatus, isPrintOrderStatus } from '../../../entities/p
 import { IPrintOrder, PrintOrderStatus } from '../../../entities/print-order/IPrintOrder';
 import { BadRequestError } from '../../../errors/BadRequestError';
 import { IPrintOrdersRepository } from '../../../repositories/print-orders/IPrintOrdersRepository';
+import { PrintOrderFilter } from '../../../shared/PrintOrderFilter';
 
 export class ListPrintOrdersUseCases {
 
@@ -9,12 +10,31 @@ export class ListPrintOrdersUseCases {
     private printOrdersRepository: IPrintOrdersRepository,
   ) { }
 
-  async execute(status?: PrintOrderStatus): Promise<IPrintOrder[]> {
+  async execute(status?: PrintOrderStatus, when?: 'today' | 'lastweek' | 'lastmonth'): Promise<IPrintOrder[]> {
     if(status && !isPrintOrderStatus(status)) {
       throw new BadRequestError('Tipo de status inválido. Os status são "in_production", "waiting", "done" e "uploading_images"');
     }
 
-    const printOrders = await this.printOrdersRepository.listAll(convertPrintOrderStatus(status));
+    let before;
+    if(when) {
+      let before;
+      if(when === 'today') {
+        before = Date.now() - 24 * 60 * 60 * 1000;
+      } else if(when === 'lastmonth') {
+        before = Date.now() - 30 * 24 * 60 * 60 * 1000;
+      } else if(when === 'lastweek') {
+        before = Date.now() - 7 * 24 * 60 * 60 * 1000;
+      } else {
+        throw new BadRequestError('Valor inválido para o parâmetro "when". Valores permitidos são "today", "lastmonth" e "lastweek"');
+      }
+    }
+
+    const filter: PrintOrderFilter = {
+      status: status ? convertPrintOrderStatus(status) : undefined,
+      when: before ? new Date(before) : undefined,
+    };
+
+    const printOrders = await this.printOrdersRepository.listAll(filter);
 
     return printOrders;
   }
