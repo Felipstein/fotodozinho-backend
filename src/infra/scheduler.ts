0f339f292983b4e77e64dec511b088ae290db848
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-20 14:26:47 -0300

    feat: deleting on node-cron expired tokens

--- src/infra/scheduler.ts
+++ src/infra/scheduler.ts
@@ -1,9 +1,21 @@
 import cron from 'node-cron';
-import { currentRevokedTokensRepository } from '../repositories';
+import { currentPasswordRecoveryTokensRepository, currentRevokedTokensRepository, currentValidatorTokensRepository } from '../repositories';
 
 const task = cron.schedule('*/30 * * * *', async () => {
   try {
     await currentRevokedTokensRepository.deleteExpiredTokens();
+  } catch (err: any) {
+    console.warn('Fail to delete revoked tokens:', err);
+  }
+
+  try {
+    await currentValidatorTokensRepository.deleteExpiredTokens();
+  } catch (err: any) {
+    console.warn('Fail to delete revoked tokens:', err);
+  }
+
+  try {
+    await currentPasswordRecoveryTokensRepository.deleteExpiredTokens();
   } catch (err: any) {
     console.warn('Fail to delete revoked tokens:', err);
   }
