6335b90b807b397f82a50b11eb2bbd78c6417fc0
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-20 02:01:09 -0300

    feat: add nodemailer email service to sign up use cases

--- src/modules/auth/signup/index.ts
+++ src/modules/auth/signup/index.ts
@@ -1,9 +1,10 @@
+import { NodemailerEmailService } from '../../../providers/emails/NodemailerEmailService';
 import { currentUsersRepository } from '../../../repositories';
 import { SignUpController } from './SignUpController';
 import { SignUpUseCases } from './SignUpUseCases';
 
 export function signUpFactory() {
-  const useCases = new SignUpUseCases(currentUsersRepository);
+  const useCases = new SignUpUseCases(currentUsersRepository, new NodemailerEmailService());
   const controller = new SignUpController(useCases);
 
   return { useCases, controller };
