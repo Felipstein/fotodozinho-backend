0fbc0c28e83dc9fd2fcc0e47600286135ba99a6d
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-20 02:30:40 -0300

    fix: removed node cron tring delete users deleted

--- src/infra/scheduler.ts
+++ src/infra/scheduler.ts
@@ -1,17 +1,11 @@
 import cron from 'node-cron';
-import { currentRevokedTokensRepository, currentUsersRepository } from '../repositories';
+import { currentRevokedTokensRepository } from '../repositories';
 
 const task = cron.schedule('*/30 * * * *', async () => {
   try {
     await currentRevokedTokensRepository.deleteExpiredTokens();
   } catch (err: any) {
     console.warn('Fail to delete revoked tokens:', err);
-  }
-
-  try {
-    await currentUsersRepository.deleteDeactivedUsersForAmonth();
-  } catch (err: any) {
-    console.warn('Fail to delete deactived users for a month:', err);
   }
 });
 
