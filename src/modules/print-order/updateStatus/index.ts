33010c9b07562d0aed6036577c2f9063dafb290f
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-02-21 16:11:07 -0300

    feat: providing services to print order use case

--- src/modules/print-order/updateStatus/index.ts
+++ src/modules/print-order/updateStatus/index.ts
@@ -1,9 +1,16 @@
-import { currentPrintOrdersRepository } from '../../../repositories';
+import { NodemailerEmailService } from '../../../providers/emails/NodemailerEmailService';
+import { currentNotificationsRepository, currentPrintOrdersRepository, currentUsersRepository } from '../../../repositories';
+import { NotificationsService } from '../../../services/notifications';
 import { UpdatePrintOrderStatusController } from './UpdatePrintOrderStatusController';
 import { UpdatePrintOrderStatusUseCases } from './UpdatePrintOrderStatusUseCases';
 
 export function updatePrintOrderStatusFactory() {
-  const updatePrintOrderStatusUseCases = new UpdatePrintOrderStatusUseCases(currentPrintOrdersRepository);
+  const updatePrintOrderStatusUseCases = new UpdatePrintOrderStatusUseCases(
+    currentPrintOrdersRepository,
+    currentUsersRepository,
+    new NotificationsService(currentNotificationsRepository),
+    new NodemailerEmailService(),
+  );
   const updatePrintOrderStatusController = new UpdatePrintOrderStatusController(updatePrintOrderStatusUseCases);
 
   return { useCases: updatePrintOrderStatusUseCases, controller: updatePrintOrderStatusController };
