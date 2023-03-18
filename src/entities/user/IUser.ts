3cb8d771f55ef949c36df4cff29aa71a9a2631fc
Felipstein <luisfelipe-oliveira@outlook.com.br>
2023-03-17 21:44:09 -0300

    fix: removed optional of phone user

--- src/entities/user/IUser.ts
+++ src/entities/user/IUser.ts
@@ -2,7 +2,7 @@ export interface IUser {
   id: string;
   name: string;
   email: string;
-  phone?: string | null;
+  phone: string | null;
   password: string;
   createdAt: Date;
   lastLogin: Date;
